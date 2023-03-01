6a5eaf1b35773585a61c04cd0141b10926af1c42
Adkirf <adkirfuglyape@gmail.com>
2023-03-01 10:07:31 +0100

    bg

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Header } from './';
+import Header from './Header';
 
-const Layout = ({ children }) => {
-  return (
-    <>
-        <Header />
-        {children}
-    </>
-  )
-}
+const Layout = ({ children }) => (
+  <>
+    <Header />
+    {children}
+  </>
+);
 
-export default Layout
+export default Layout;
